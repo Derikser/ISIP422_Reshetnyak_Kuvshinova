8b3df1e2827a8954129965a013f8ec3052239e7a
derikser <denr60106@gmail.com>
2025-09-22 13:50:36 +0300

    Создан базовый класс Product и перечисление Category

---
+++ ISIP422_Reshetnyak_Kuvshinova/Product.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace StoreInventoryApp
+{
+    public class Product : INotifyPropertyChanged
+    {
+        private static int _lastProductId = 0;
+
+        public Product()
+        {
+            _lastProductId++;
+            Id = _lastProductId;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public bool IsInStock => Quantity > 0;
+        public Category Category { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+
+    public enum Category
+    {
+        Electronics,
+        Food,
+        Clothing
+    }
+}
