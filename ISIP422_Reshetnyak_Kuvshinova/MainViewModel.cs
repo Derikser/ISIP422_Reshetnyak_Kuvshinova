bc733d7316d36ea00e1734537997f20ff4203660
derikser <denr60106@gmail.com>
2025-09-22 14:30:57 +0300

    Добавлены команды для добавления и удаления товаров

--- ISIP422_Reshetnyak_Kuvshinova/MainViewModel.cs
+++ ISIP422_Reshetnyak_Kuvshinova/MainViewModel.cs
@@ -16,6 +16,10 @@ namespace ISIP422_Reshetnyak_Kuvshinova
         public MainViewModel()
         {
             Products = new ObservableCollection<Product>();
+
+            // Команды
+            AddProductCommand = new RelayCommand(AddProduct);
+            DeleteProductCommand = new RelayCommand(DeleteProduct, CanDeleteProduct);
         }
 
         public ObservableCollection<Product> Products
@@ -36,6 +40,36 @@ namespace ISIP422_Reshetnyak_Kuvshinova
                 _selectedProduct = value;
                 OnPropertyChanged(nameof(SelectedProduct));
             }
+        }
+
+        // Команды
+        public RelayCommand AddProductCommand { get; }
+        public RelayCommand DeleteProductCommand { get; }
+
+        private void AddProduct()
+        {
+            var newProduct = new Product()
+            {
+                Name = "Новый товар",
+                Price = 100,
+                Quantity = 0,
+                Category = Category.Electronics
+            };
+            Products.Add(newProduct);
+            SelectedProduct = newProduct;
+        }
+
+        private void DeleteProduct()
+        {
+            if (SelectedProduct != null)
+            {
+                Products.Remove(SelectedProduct);
+            }
+        }
+
+        private bool CanDeleteProduct()
+        {
+            return SelectedProduct != null;
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
