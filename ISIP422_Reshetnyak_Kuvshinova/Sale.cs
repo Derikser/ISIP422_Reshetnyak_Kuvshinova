fa1f713438a523987eae2f1e4a11b9f6587eed1b
derikser <denr60106@gmail.com>
2025-09-22 14:45:49 +0300

    ДОП. ЗАДАНИЕ: Добавлен класс Sale для учета истории продаж

---
+++ ISIP422_Reshetnyak_Kuvshinova/Sale.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISIP422_Reshetnyak_Kuvshinova
+{
+    public class Sale
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public int ProductId { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public decimal TotalAmount => Price * Quantity;
+        public DateTime SaleDate { get; set; }
+
+        public Sale()
+        {
+            SaleDate = DateTime.Now;
+        }
+
+        public override string ToString()
+        {
+            return $"{ProductName} (Код: {ProductId}) - {Quantity} шт. × {Price:C} = {TotalAmount:C} - {SaleDate:dd.MM.yyyy HH:mm}";
+        }
+    }
+}
